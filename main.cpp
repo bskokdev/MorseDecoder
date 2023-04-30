9de827d2f8df241b5bf5c7163bc608da1710c508
Boris Skok <skokcmd@gmail.com>
2023-04-30 14:04:38 +0200

    reading input & decoding string

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "MorseDecoder.h"
 
-int main(int argc, char* argv[]) {
+int main() {
+    // get encoded string from cin
+    std::string encodedString;
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        if (line.empty()) {
+            break;
+        }
+        encodedString += line;
+    }
+
+    MorseDecoder decoder;
+    std::cout << decoder.decodeMessage(encodedString) << std::endl;
     return 0;
 }
