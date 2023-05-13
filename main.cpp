7baf998507afcff40ea46f2c68988a6384f8e984
Boris Skok <skokcmd@gmail.com>
2023-05-13 16:30:23 +0200

    reading from file instead of cin & added input.txt

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <fstream>
 #include "MorseDecoder.h"
 
-int main() {
-    // get encoded string from cin
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << "Invalid arguments" << std::endl;
+    }
+
+    // read encoded string from file
+    std::string file = argv[1];
     std::string encodedString;
-    std::string line;
-    while (std::getline(std::cin, line)) {
-        if (line.empty()) {
-            break;
-        }
-        encodedString += line;
+    std::ifstream inputFile(file);
+    if (inputFile.is_open()) {
+        std::getline(inputFile, encodedString);
+    } else {
+        std::cout << "Could not open file" << std::endl;
     }
 
     MorseDecoder decoder;
